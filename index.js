3a2114244c5a1bbd4cf2494c805f521bf61e4578
deko possas <andrepossaster@gmail.com>
2021-05-01 20:26:07 -0300

    gtet all posts

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
-const { posts, tags } = require('./models');
-
 const app = express();
 
-app.get('/employees', (_req, res) => Employees
-  .findAll({ include: { model: Addresses, as: 'addresses' } })
-  .then((answer) => res.status(200).json(answer))
-  .catch(() => res.status(500).json({ message: 'Algo deu errado' })));
+const { posts, tags } = require('./models');
+
+app.use(express.json());
+
+app.get('/posts', (_req, res) => {
+  try {
+    const post = await Post.findAll({
+      include: [
+        {
+          model: Tag,
+          as: 'tags',
+          througth : { attributes: [] },
+        },
+      ],
+    });
+
+    return res.status(200).json(posts);
+  } catch (err) {
+    return res.status(500).json({ err });
+  }
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Port: ${PORT}`));
